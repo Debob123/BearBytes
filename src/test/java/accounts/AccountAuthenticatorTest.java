dec83e01ca6c620679091ced794f492856772a96
juanswolo <lukeheard08@gmail.com>
2024-03-20 13:44:37 -0500

    added dependencies

---
+++ src/test/java/accounts/AccountAuthenticatorTest.java
@@ -0,0 +1,10 @@
+package accounts;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountAuthenticatorTest {
+    protected static AccountAuthenticator auth;
+}
