8d2b4e1d7b0fef019ab029a7c70738f0aa618ea2
Caldwell <noah_caldwell2@baylor.edu>
2024-03-20 14:05:04 -0500

    added getters and setters to Bill class

--- src/main/java/reservation/system/Bill.java
+++ src/main/java/reservation/system/Bill.java
@@ -1,2 +1,9 @@
-package reservation.system;public class Bill {
+package reservation.system;
+import java.util.Calendar;
+public class Bill {
+    private static int billID;
+    private double amount;
+    private Calendar dateIssued;
+    private String customerName;
+
 }
