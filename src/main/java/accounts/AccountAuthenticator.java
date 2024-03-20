00222f722731a4fbb9353c59999f3cad9521543d
juanswolo <lukeheard08@gmail.com>
2024-03-20 14:39:06 -0500

    added AccAuth functions and tests; dependencies

--- src/main/java/accounts/AccountAuthenticator.java
+++ src/main/java/accounts/AccountAuthenticator.java
@@ -1,6 +1,8 @@
 package accounts;
 
 import org.mindrot.jbcrypt.BCrypt;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AccountAuthenticator {
     public AccountAuthenticator() {}
@@ -12,5 +14,21 @@ public class AccountAuthenticator {
     // checks a password against the hash using BCrypt
     public static boolean verifyPassword(String password, String hashedPassword) {
         return BCrypt.checkpw(password, hashedPassword);
+    }
+    // checks if password meets requirements
+    public static boolean goodPassword(String password) {
+        // 8 or more characters
+        if (password.length() < 8) { return false; }
+        // first character is a letter
+        if (!Character.isLetter(password.charAt(0))) { return false; }
+        // has no spaces
+        if (password.contains(" ")) { return false; }
+        // contains a digit and a special character
+        Pattern digit = Pattern.compile("[0-9]");
+        Pattern special = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        Matcher hasDigit = digit.matcher(password);
+        Matcher hasSpecial = special.matcher(password);
+        // returns if it has digit and special
+        return hasDigit.find() && hasSpecial.find();
     }
 }
--- src/test/java/accounts/AccountAuthenticatorTest.java
+++ src/test/java/accounts/AccountAuthenticatorTest.java
@@ -1,10 +1,29 @@
 package accounts;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AccountAuthenticatorTest {
-    protected static AccountAuthenticator auth;
+
+    // tests if strings passed to hash are correctly verified
+    @ParameterizedTest
+    @ValueSource(strings = {"password", "a", "z", "0", "ThisIsAStrongPassword900!"})
+    void hashToVerify(String password) {
+        String hashed = AccountAuthenticator.hashPassword(password);
+        assert(AccountAuthenticator.verifyPassword(password, hashed));
+    }
+    // tests if passing passwords actually passes
+    @ParameterizedTest
+    @ValueSource(strings = {"ThisIsAGood1!", "also_0_a_0_good_password", "Z234567!"})
+    void passingPasswords(String password) {
+        assert(AccountAuthenticator.goodPassword(password));
+    }
+    // tests if failing passwords actually fails
+    @ParameterizedTest
+    @ValueSource(strings = {"1badpassword!","short1!",
+            "_badpassword1","hasNoNumber!","hasNoSpecial1","has a space1!"})
+    void failingPasswords(String password) {
+        assert(!AccountAuthenticator.goodPassword(password));
+    }
 }
