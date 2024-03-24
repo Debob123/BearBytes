b42dbf1812ca1e6e352be514d264327c97eed93c
Caldwell <noah_caldwell2@baylor.edu>
2024-03-24 18:09:18 -0500

    added setters and attributes to receipt class

--- src/main/java/billing/Reciept.java
+++ src/main/java/billing/Reciept.java
@@ -1,5 +1,40 @@
 package billing;
 
-public class Reciept {
+public class Reciept { // Reservation details
+private String confirmationNumber;
+private String checkInDate;
+private String checkOutDate;
+private String roomType;
+private int numberOfRooms;
 
+// Guest Information
+private String guestName;
+private String contactInformation;
+
+// Transaction details
+private double totalAmount;
+private String paymentMethod;
+private String billingAddress;
+
+// Cancellation and refund policy
+private String cancellationPolicy;
+
+// Loyalty points earned
+private int loyaltyPointsEarned;
+
+// Additional information
+private String promotionalOffers;
+
+    public void setConfirmationNumber(String s){confirmationNumber = s;}
+    public void setCheckInDate(String s){checkInDate = s;}
+    public void setCheckOutDate(String s){checkOutDate = s;}
+    public void setRoomType(String s){roomType = s;}
+    public void setNumberOfRooms(int n){numberOfRooms = n;}
+public void setTotalAmount(double d){totalAmount = d;}
+public void setPaymentMethod(String s){paymentMethod = s;}
+public void setBillingAddress(String s){ billingAddress = s;}
+
+public void setCancellationPolicy(String s){cancellationPolicy = s;}
+public void setLoyaltyPointsEarned(int n){loyaltyPointsEarned = n;}
+public void setPromotionalOffers(String s){promotionalOffers = s;}
 }
