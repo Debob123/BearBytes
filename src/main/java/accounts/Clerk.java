0f99aace61189d930c1611e93d0f49650733a662
AIparks913 <andrewparks913@icloud.com>
2024-03-24 23:12:33 -0500

    Added additional getters/setters for Clerks and Guests

--- src/main/java/accounts/Clerk.java
+++ src/main/java/accounts/Clerk.java
@@ -1,7 +1,17 @@
 package accounts;
 
 public class Clerk extends Account {
+    private String profileInfo;
+
     public Clerk(String username, String password) {
         super(username, password);
+    }
+
+    public void setProfileInfo(String info){
+        profileInfo = info;
+    }
+
+    public String getProfileInfo(){
+        return profileInfo;
     }
 }
--- src/main/java/accounts/Guest.java
+++ src/main/java/accounts/Guest.java
@@ -1,7 +1,42 @@
 package accounts;
 
 public class Guest extends Account {
+    String name, address, creditCardExpiration;
+    long creditCardNumber;
+
     public Guest(String username, String password) {
         super(username, password);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setCreditCardNumber(long number){
+        creditCardNumber = number;
+    }
+
+    public long getCreditCardNumber(){
+        return creditCardNumber;
+    }
+
+    public void setCreditCardExpiration(String exp){
+        creditCardExpiration = exp;
+    }
+
+    public String getCreditCardExpiration(){
+        return creditCardExpiration;
     }
 }
