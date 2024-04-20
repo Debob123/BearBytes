b8ea028d525f050ed612256910f1f4bcb2c83548
Jake Mauldin <jake_mauldin1@baylor.edu>
2024-04-20 18:54:02 -0500

    fixed bug, working search bar

--- src/main/java/bearbytes/dev/hotel/controllers/OrderController.java
+++ src/main/java/bearbytes/dev/hotel/controllers/OrderController.java
@@ -2,9 +2,12 @@ package bearbytes.dev.hotel.controllers;
 
 import bearbytes.dev.hotel.checkout.Order;
 import bearbytes.dev.hotel.database.OrderDAO;
+import bearbytes.dev.hotel.database.ReservationDAO;
 import bearbytes.dev.hotel.product.Product;
 import org.springframework.web.bind.annotation.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/shop")
@@ -24,5 +27,27 @@ public class OrderController {
             System.out.println("order not processed");
         }
         return false;
+    }
+
+    @RestController
+    @RequestMapping("/shop")
+    public static class ProductController {
+        ReservationDAO.ProductDAO productDAO;
+
+        public ProductController() {
+            productDAO = new ReservationDAO.ProductDAO();
+        }
+
+        @CrossOrigin(origins = "http://localhost:3000")
+        @PostMapping("/getProducts")
+        public Collection<Product> getProducts() {
+            try {
+                return productDAO.getProducts();
+            } catch(Exception e) {
+                System.out.println("API fail");
+                e.printStackTrace();
+            }
+            return new ArrayList<>();
+        }
     }
 }
--- src/main/java/bearbytes/dev/hotel/database/ReservationDAO.java
+++ src/main/java/bearbytes/dev/hotel/database/ReservationDAO.java
@@ -2,6 +2,7 @@ package bearbytes.dev.hotel.database;
 
 import bearbytes.dev.hotel.floor.Room;
 import bearbytes.dev.hotel.interfaces.IReservationDAO;
+import bearbytes.dev.hotel.product.Product;
 import bearbytes.dev.hotel.reservation.Reservation;
 
 import java.sql.*;
@@ -185,5 +186,61 @@ public class ReservationDAO implements IReservationDAO {
             System.out.println(e.getMessage());
         }
         return dbConnection;
+    }
+
+    public static class ProductDAO  {
+        private static final String DB_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+        private static final String DB_CONNECTION = "jdbc:derby:myDB";
+        private static final String DB_USER = "";
+        private static final String DB_PASSWORD = "";
+
+        public Collection<Product> getProducts() throws SQLException  {
+            Connection dbConnection = null;
+            Statement statement = null;
+            String getListedProductsSQL = "SELECT * FROM Products";
+            List<Product> products = new ArrayList<Product>();
+            try  {
+                dbConnection = connectToDatabase();
+                statement = dbConnection.createStatement();
+                ResultSet rs = statement.executeQuery(getListedProductsSQL);
+                while(rs.next())  {
+                    Product p = new Product(
+                            rs.getInt("productID"),
+                            rs.getString("name"),
+                            rs.getDouble("price"),
+                            rs.getString("image")
+                    );
+                    products.add(p);
+                }
+            } catch(SQLException e)  {
+                e.printStackTrace();
+                return null;
+            } finally  {
+                if(statement != null)  {
+                    statement.close();
+                }
+                if(dbConnection != null)  {
+                    dbConnection.close();
+                }
+            }
+            return products;
+        }
+        private static Connection connectToDatabase () throws SQLException {
+            Connection dbConnection = null;
+            try {
+                Class.forName(DB_DRIVER);
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+            try {
+                dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+                System.out.println("Connected");
+                return dbConnection;
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            return dbConnection;
+        }
+
     }
 }
