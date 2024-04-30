94c2752bbc4145dff4ee7b8b8448cff5439e400f
Blaz1c <32022290+BigBlaze7@users.noreply.github.com>
2024-04-30 14:03:29 -0500

    clerkMakeReservation date select update

--- reactFrontEnd/src/pages/ClerkMakeReservation.jsx
+++ reactFrontEnd/src/pages/ClerkMakeReservation.jsx
@@ -8,7 +8,7 @@ import "./styles/clerkModifyReservations.css"
 
 function ClerkMakeReservation() {
     const navigate = useNavigate();
-    const button = <Button text="Reserve"/>
+    const button = <Button text="Reserve" />
     const [rooms, setRooms] = useState([]);
     const [addedRooms, setAddedRooms] = useState([]);
     const [submitted, setSubmitted] = useState(false);
@@ -16,35 +16,46 @@ function ClerkMakeReservation() {
     const [end, setEnd] = useState('');
 
     // Fetch all of the available rooms
-    function renderRooms() {
+    const renderRooms = (e) => {
+        e.preventDefault();
         setAddedRooms([]);
         let currDate = new Date();
-        let startingDate = new Date(start + "T00:00-0500")
-        let endingDate = new Date(end + "T00:00-0500");
-        if(!isNaN(startingDate) && !isNaN(endingDate) && end > start && startingDate > currDate) {
+        let startingDate = new Date(start + "T00:00:00-0500");
+        let endingDate = new Date(end + "T00:00:00-0500");
+
+        startingDate.setMinutes(startingDate.getMinutes() - startingDate.getTimezoneOffset());
+        endingDate.setMinutes(endingDate.getMinutes() - endingDate.getTimezoneOffset());
+
+        console.log("Button Pressed!");
+
+
+
+        console.log(`Start date: ${startingDate}, End date: ${endingDate}`);
+
+        if (!isNaN(startingDate) && !isNaN(endingDate) && end > start && startingDate > currDate) {
             let dateRange = [start, end]
             sessionStorage.setItem('dates', JSON.stringify(dateRange));
-        
+
             fetch('http://localhost:8080/room/getAvailableRooms', {
                 mode: 'cors',
                 method: 'POST',
                 headers: {
-                  'Content-Type': 'application/json',
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(dateRange)
             })
-            .then(response => response.json())
-            .then(data => {
-                // Set the array of room objects
-                setRooms(data);
-                setSubmitted(true);
-            })
-            .catch(error => console.error('Error creating room array:', error));
+                .then(response => response.json())
+                .then(data => {
+                    // Set the array of room objects
+                    setRooms(data);
+                    setSubmitted(true);
+                })
+                .catch(error => console.error('Error creating room array:', error));
         }
     }
 
     const handleConfirm = () => {
-        if(addedRooms.length !== 0) {
+        if (addedRooms.length !== 0) {
             sessionStorage.setItem('rooms', JSON.stringify(addedRooms));
             navigate("/clerkConfirmReservation")
         }
@@ -52,45 +63,45 @@ function ClerkMakeReservation() {
 
     const displayRooms = () => {
         return (
-            submitted && 
-            (rooms.length !== 0 ? 
-            <div>
-                <div className="display">
-                    {rooms.map((room) =>
-                        <BoxDisplay className="box-display" 
-                            key={room.number} 
-                            imgLink={singleRoom} 
-                            type={room.type} 
-                            cost={room.dailyRate} 
-                            numBeds={room.numBeds}
-                            bedSize={room.bedSize}
-                            smokingAllowed={room.smokingAllowed}
-                            addedRooms={addedRooms}
-                            setAddedRooms={setAddedRooms}
-                            currRooms={rooms}
-                            setCurrRooms={setRooms}
-                            roomNum={room.number}
-                            quality={room.quality}
-                            btn={button}/> 
+            submitted &&
+            (rooms.length !== 0 ?
+                <div>
+                    <div className="display">
+                        {rooms.map((room) =>
+                            <BoxDisplay className="box-display"
+                                key={room.number}
+                                imgLink={singleRoom}
+                                type={room.type}
+                                cost={room.dailyRate}
+                                numBeds={room.numBeds}
+                                bedSize={room.bedSize}
+                                smokingAllowed={room.smokingAllowed}
+                                addedRooms={addedRooms}
+                                setAddedRooms={setAddedRooms}
+                                currRooms={rooms}
+                                setCurrRooms={setRooms}
+                                roomNum={room.number}
+                                quality={room.quality}
+                                btn={button} />
                         )
-                    }
-                </div>
-                <div className="footer">
-                    <button className="footer-btn" onClick={handleRemove}>Remove Last Room Added</button>
-                    <button className="footer-btn" onClick={handleConfirm}>Confirm Reservation</button>
-                </div>
-            </div> :
-            <p>There are no rooms available on these dates</p>)
+                        }
+                    </div>
+                    <div className="footer">
+                        <button className="footer-btn" onClick={handleRemove}>Remove Last Room Added</button>
+                        <button className="footer-btn" onClick={handleConfirm}>Confirm Reservation</button>
+                    </div>
+                </div> :
+                <p>There are no rooms available on these dates</p>)
         )
     }
 
     const handleRemove = () => {
-        if(addedRooms.length > 0) {
+        if (addedRooms.length > 0) {
             let r = addedRooms.pop();
-   
+
             let newRooms = [...rooms];
             newRooms.push(r);
-            newRooms.sort((a,b) => a.number - b.number);
+            newRooms.sort((a, b) => a.number - b.number);
             setRooms(newRooms);
             sessionStorage.setItem('rooms', JSON.stringify(addedRooms));
         }
@@ -98,28 +109,24 @@ function ClerkMakeReservation() {
 
     return (
         <div>
-            <ClerkNavigation/>
+            <ClerkNavigation />
             <div className="content">
                 <h1 className="center-text" style={{ fontSize: '4em' }} >Guest Reservation</h1>
                 <div className="center-input top-space">
                     <label htmlFor="start_date">Start date:</label>
-                    <input 
-                        type="text" 
-                        id="start_date" 
+                    <input
+                        type="date"
+                        id="start_date"
                         name="startDate"
-                        placeholder="YYYY-MM-DD" 
-                        pattern="\d{4}-\d{2}-\d{2}"
                         onChange={(e) => setStart(e.target.value)}
-                        required/>
+                        required />
                     <label htmlFor="end_date">End date:</label>
-                    <input 
-                        type="text" 
-                        id="end_date" 
-                        name="endDate" 
-                        placeholder="YYYY-MM-DD" 
-                        pattern="\d{4}-\d{2}-\d{2}"
+                    <input
+                        type="date"
+                        id="end_date"
+                        name="endDate"
                         onChange={(e) => setEnd(e.target.value)}
-                        required/>
+                        required />
                     <button className="submit-btn" onClick={renderRooms}>Submit</button>
                 </div>
                 {displayRooms()}
