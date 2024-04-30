d5b9df0a87704761223a2d018addae06d664605b
Blaz1c <32022290+BigBlaze7@users.noreply.github.com>
2024-04-30 14:59:23 -0500

    added use case for a clerk finding all Reservations

--- reactFrontEnd/src/pages/ClerkModifyReservations.jsx
+++ reactFrontEnd/src/pages/ClerkModifyReservations.jsx
@@ -8,9 +8,33 @@ function ClerkModifyReservation() {
     const [username, setUsername] = useState('');
     const [reservations, setReservations] = useState([]);
     const [submitted, setSubmitted] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+
+    function fetchAllReservations() {
+        setSubmitted(true);
+        setLoading(true);
+        fetch('http://localhost:8080/reservation/getAllReservations', {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                setReservations(data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching all reservations:', error);
+                setLoading(false);
+            });
+    }
 
     function renderReservations() {
-        if(username) {
+        if (username) {
+            setLoading(true);
             fetch('http://localhost:8080/reservation/getReservations', {
                 mode: 'cors',
                 method: 'POST',
@@ -19,50 +43,58 @@ function ClerkModifyReservation() {
                 },
                 body: JSON.stringify(username)
             })
-            .then(response => response.json())
-            .then(data => {
-                // Set the array of reservations
-                setReservations(data);
-            })
-            .catch(error => console.error('Error creating room array:', error));
+                .then(response => response.json())
+                .then(data => {
+                    setReservations(data);
+                    setLoading(false);
+                })
+                .catch(error => {
+                    console.error('Error creating room array:', error);
+                    setLoading(false);
+                });
             setSubmitted(true);
         }
     }
 
     const displayReservations = () => {
-        return (
-            submitted && 
-            (reservations.length !== 0 ? 
-            <div className="display">
-                {
-                reservations.map((reservation) =>(
-                    <ReservationDisplay className="box-display" 
-                        key={reservation.reservationID} 
-                        imgLink={singleRoom} 
-                        reservation={reservation}
-                        reservations={reservations}
-                        setReservations={setReservations}
-                        />
-                ))}
-            </div> : 
-            <p className="center-text">This guest does not have a reservation</p> )
-        )
+        if (loading) {
+            return <p className="center-text">Loading...</p>;
+        } else {
+            return (
+                submitted &&
+                (reservations.length !== 0 ?
+                    <div className="display">
+                        {
+                            reservations.map((reservation) => (
+                                <ReservationDisplay className="box-display"
+                                    key={reservation.reservationID}
+                                    imgLink={singleRoom}
+                                    reservation={reservation}
+                                    reservations={reservations}
+                                    setReservations={setReservations}
+                                />
+                            ))}
+                    </div> :
+                    <p className="center-text">This guest does not have a reservation</p>)
+            )
+        }
     }
 
     return (
         <div>
-            <ClerkNavigation/>
+            <ClerkNavigation />
             <div className="content">
                 <h1 className="center-text">Guest Reservation</h1>
                 <div className="center-input top-space">
                     <label htmlFor="username">Enter guest username:</label>
-                    <input type="text" 
-                    id="username" 
-                    name="username" 
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)} 
-                    placeholder="Username" />
+                    <input type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Username" />
                     <button className="submit-btn" onClick={renderReservations}>Submit</button>
+                    <button className="submit-btn" onClick={fetchAllReservations}>See All Reservations</button>
                 </div>
                 {displayReservations()}
             </div>
--- src/main/java/bearbytes/dev/hotel/database/ReservationDAO.java
+++ src/main/java/bearbytes/dev/hotel/database/ReservationDAO.java
@@ -27,11 +27,12 @@ public class ReservationDAO implements IReservationDAO {
      *
      * @param r The reservation seeking to be created.
      * @return All room numbers that match the given requirements.
-     * @throws SQLException If a database access error occurs.
-     * @throws InvalidArgumentException If the passed reservation does not have valid data
+     * @throws SQLException             If a database access error occurs.
+     * @throws InvalidArgumentException If the passed reservation does not have
+     *                                  valid data
      */
     public Collection<Integer> checkAvailability(Reservation r) throws SQLException, InvalidArgumentException {
-        if(r == null) {
+        if (r == null) {
             throw new InvalidArgumentException("The reservation cannot be null");
         }
         // Now try to connect
@@ -70,19 +71,21 @@ public class ReservationDAO implements IReservationDAO {
      *
      * @param reservation The reservation to add to the database.
      * @return True if the reservation was successfully added, else false.
-     * @throws SQLException If a database access error occurs.
-     * @throws InvalidArgumentException If the passed reservation does not have valid data
+     * @throws SQLException             If a database access error occurs.
+     * @throws InvalidArgumentException If the passed reservation does not have
+     *                                  valid data
      */
     public boolean add(Reservation reservation) throws SQLException, InvalidArgumentException {
-        if(reservation == null) {
+        if (reservation == null) {
             throw new InvalidArgumentException("The reservation cannot be null");
         } else if (reservation.getRooms() == null || reservation.getRooms().isEmpty()) {
             throw new InvalidArgumentException("The list of rooms cannot be null or empty");
-        } else if(reservation.getUsername() == null) {
+        } else if (reservation.getUsername() == null) {
             throw new InvalidArgumentException("This Guest's username is mandatory ");
         }
 
-        if(!AccountAuthenticator.validateGuestUsername(new Guest(reservation.getUsername(), "", null, null, null, null))) {
+        if (!AccountAuthenticator
+                .validateGuestUsername(new Guest(reservation.getUsername(), "", null, null, null, null))) {
             throw new InvalidArgumentException("There is no guest with given username: " + reservation.getUsername());
         }
         // Now try to connect
@@ -136,12 +139,13 @@ public class ReservationDAO implements IReservationDAO {
      *
      * @param reservations The list of reservations to modify.
      * @return A list of room numbers that match the given criteria.
-     * @throws SQLException If a database access error occurs.
-     * @throws InvalidArgumentException If the passed reservations do not have valid data
+     * @throws SQLException             If a database access error occurs.
+     * @throws InvalidArgumentException If the passed reservations do not have valid
+     *                                  data
      */
     public Collection<Integer> modify(Reservation[] reservations) throws SQLException, InvalidArgumentException {
-        List<Integer> rooms = (ArrayList)checkAvailability(reservations[1]);
-        if(rooms.isEmpty()) {
+        List<Integer> rooms = (ArrayList) checkAvailability(reservations[1]);
+        if (rooms.isEmpty()) {
             remove(reservations[0]);
             add(reservations[1]);
         }
@@ -154,18 +158,20 @@ public class ReservationDAO implements IReservationDAO {
      *
      * @param r The reservation to remove.
      * @return True if the reservation is successfully removed, else false.
-     * @throws SQLException If a database access error occurs.
-     * @throws InvalidArgumentException If the passed reservation does not have valid data
+     * @throws SQLException             If a database access error occurs.
+     * @throws InvalidArgumentException If the passed reservation does not have
+     *                                  valid data
      */
     public boolean remove(Reservation r) throws SQLException, InvalidArgumentException {
-        if(r == null) {
+        if (r == null) {
             throw new InvalidArgumentException("The reservation cannot be null");
         }
         Connection c = getDBConnection();
         PreparedStatement ps = null;
 
         try {
-            // Apache Derby does not support deleting from multiple tables, so must use two statements
+            // Apache Derby does not support deleting from multiple tables, so must use two
+            // statements
             // Delete from reservations table
             String query = "DELETE FROM APP.Reservations r WHERE r.reservationID = ?";
             ps = c.prepareStatement(query);
@@ -196,18 +202,20 @@ public class ReservationDAO implements IReservationDAO {
      *
      * @param r The reservation to cancel.
      * @return True if the reservation is successfully cancelled, else false.
-     * @throws SQLException If a database access error occurs.
-     * @throws InvalidArgumentException If the passed reservation does not have valid data
+     * @throws SQLException             If a database access error occurs.
+     * @throws InvalidArgumentException If the passed reservation does not have
+     *                                  valid data
      */
     public boolean cancel(Reservation r) throws SQLException, InvalidArgumentException {
-        if(r == null) {
+        if (r == null) {
             throw new InvalidArgumentException("The reservation cannot be null");
         }
         Connection c = getDBConnection();
         PreparedStatement ps = null;
 
         try {
-            // Apache Derby does not support deleting from multiple tables, so must use two statements
+            // Apache Derby does not support deleting from multiple tables, so must use two
+            // statements
             // Edit the reservations table
             String query = "UPDATE APP.Reservations SET status = ? WHERE reservationID = ?";
             ps = c.prepareStatement(query);
@@ -240,7 +248,8 @@ public class ReservationDAO implements IReservationDAO {
         Connection c = getDBConnection();
         PreparedStatement ps = null;
         List<Reservation> reservations = new ArrayList<>();
-        if(username.startsWith("\"") && username.endsWith("\"") || username.startsWith("\'") && username.endsWith("\'")) {
+        if (username.startsWith("\"") && username.endsWith("\"")
+                || username.startsWith("\'") && username.endsWith("\'")) {
             username = username.substring(1, username.length() - 1);
         }
 
@@ -293,7 +302,8 @@ public class ReservationDAO implements IReservationDAO {
                 resRooms.put(reservationID, rooms);
 
                 if (!prevResID.equals(reservationID)) {
-                    reservations.add(new Reservation(prevResID, resRooms.get(prevResID), prevStart, prevEnd, username, null));
+                    reservations.add(
+                            new Reservation(prevResID, resRooms.get(prevResID), prevStart, prevEnd, username, null));
                 }
             }
             if (hasReservations) {
@@ -310,6 +320,85 @@ public class ReservationDAO implements IReservationDAO {
             }
         }
 
+        return reservations;
+    }
+
+    /**
+     * Obtains all reservation from the database.
+     *
+     * @return A collection of all the current reservations.
+     * @throws SQLException If a database access error occurs.
+     */
+    public Collection<Reservation> getAllRes() throws SQLException {
+        Connection c = getDBConnection();
+        PreparedStatement ps = null;
+        List<Reservation> reservations = new ArrayList<>();
+
+        try {
+            // Prepare the query's
+            String query = "SELECT * FROM Reservations r JOIN Bookings b ON r.reservationID = b.reservationID " +
+                    "JOIN Rooms rm ON rm.roomNumber = b.roomNumber WHERE r.status != ? ORDER BY b.reservationID DESC";
+            ps = c.prepareStatement(query);
+
+            // Retrieve the reservations
+            ps.setString(1, "CANCELLED");
+            ResultSet rs = ps.executeQuery();
+
+            // Add the rooms to the reservation
+            Map<Integer, List<Room>> resRooms = new HashMap<>();
+            Integer prevResID;
+            Integer reservationID = null;
+            String prevStart;
+            String prevEnd;
+            boolean hasReservations = false;
+            String start = null, end = null;
+            while (rs.next()) {
+                prevResID = reservationID;
+                prevStart = start;
+                prevEnd = end;
+                reservationID = rs.getInt("reservationID");
+                start = rs.getString("startDate");
+                end = rs.getString("endDate");
+                if (prevResID == null && prevStart == null && prevEnd == null) {
+                    prevResID = reservationID;
+                    prevStart = start;
+                    prevEnd = end;
+                    hasReservations = true;
+                }
+                // Get room information
+                Integer roomNumber = rs.getInt("roomNumber");
+                Integer floor = rs.getInt("floor");
+                Integer numBeds = rs.getInt("numBeds");
+                Double dailyRate = rs.getDouble("dailyRate");
+                Boolean smokingAllowed = rs.getBoolean("smokingAllowed");
+                String bedSize = rs.getString("bedSize");
+                String type = rs.getString("type");
+                String quality = rs.getString("quality");
+
+                List<Room> rooms = resRooms.getOrDefault(reservationID, new ArrayList<Room>());
+                rooms.add(new Room(roomNumber, floor, numBeds, dailyRate, smokingAllowed,
+                        Room.BedType.getEnum(bedSize), Room.RoomType.getEnum(type),
+                        Room.QualityLevel.getEnum(quality)));
+                resRooms.put(reservationID, rooms);
+
+                if (!prevResID.equals(reservationID)) {
+                    reservations.add(new Reservation(prevResID, resRooms.get(prevResID), prevStart, prevEnd, null,
+                            null));
+                }
+            }
+            if (hasReservations) {
+                reservations.add(new Reservation(reservationID, resRooms.get(reservationID), start, end, null, ""));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (c != null) {
+                c.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+        }
         return reservations;
     }
 
