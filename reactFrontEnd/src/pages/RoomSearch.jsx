9feaa9eef5598c292d4793d1a5443e201a03acfc
Calrus <devan_sengel1@baylor.edu>
2024-04-29 21:53:57 -0500

    added calendar picker

--- reactFrontEnd/src/pages/RoomSearch.jsx
+++ reactFrontEnd/src/pages/RoomSearch.jsx
@@ -4,6 +4,8 @@ import './styles/roomSearch.css'
 import GuestNavigation from '../components/GuestNavigation';
 
 
+
+
 function RoomSearch() {
     const navigate = useNavigate();
     const [start, setStart] = useState('');
@@ -12,45 +14,52 @@ function RoomSearch() {
     const handleDateConfirm = (e) => {
         e.preventDefault();
         let currDate = new Date();
-        let startingDate = new Date(start + "T00:00-0500")
-        let endingDate = new Date(end + "T00:00-0500");
-        if(!isNaN(startingDate) && !isNaN(endingDate) && end > start && startingDate > currDate) {
+        let startingDate = new Date(start);
+        let endingDate = new Date(end);
+
+        startingDate.setMinutes(startingDate.getMinutes() - startingDate.getTimezoneOffset());
+        endingDate.setMinutes(endingDate.getMinutes() - endingDate.getTimezoneOffset());
+
+        console.log("Button Pressed!");
+
+        console.log(`Start date: ${startingDate}, End date: ${endingDate}`); // Add this line
+
+
+
+        if (!isNaN(startingDate) && !isNaN(endingDate) && end > start && startingDate > currDate) {
             let dateRange = [start, end]
             sessionStorage.setItem('dates', JSON.stringify(dateRange));
-            navigate('/reservation')
+            console.log("Dates confirmed!");
+            navigate('/reservation');
         }
     }
 
     return (
         <div>
-            <GuestNavigation/>
+            <GuestNavigation />
             <div className="content">
-            <h1 className="center bottom-gap">Choose dates for your stay</h1>
-            <form className="form-grid">
-                <div className="grid-element">
-                    <label htmlFor="start_date">Start date:</label>
-                    <input 
-                        type="text" 
-                        id="start_date" 
-                        name="startDate"
-                        placeholder="YYYY-MM-DD" 
-                        pattern="\d{4}-\d{2}-\d{2}"
-                        onChange={(e) => setStart(e.target.value)}
-                        required/>
-                </div>
-                <div className="grid-element">
-                    <label htmlFor="end_date">End date:</label>
-                    <input 
-                        type="text" 
-                        id="end_date" 
-                        name="endDate" 
-                        placeholder="YYYY-MM-DD" 
-                        pattern="\d{4}-\d{2}-\d{2}"
-                        onChange={(e) => setEnd(e.target.value)}
-                        required/>
-                </div>
-                <button className="grid-element grid-btn" onClick={handleDateConfirm}>Confirm Dates</button>
-            </form>
+                <h1 className="center bottom-gap">Choose dates for your stay</h1>
+                <form className="form-grid">
+                    <div className="grid-element">
+                        <label htmlFor="start_date">Start date:</label>
+                        <input
+                            type="date"
+                            id="start_date"
+                            name="startDate"
+                            onChange={(e) => setStart(e.target.value)}
+                            required />
+                    </div>
+                    <div className="grid-element">
+                        <label htmlFor="end_date">End date:</label>
+                        <input
+                            type="date"
+                            id="end_date"
+                            name="endDate"
+                            onChange={(e) => setEnd(e.target.value)}
+                            required />
+                    </div>
+                    <button className="grid-element grid-btn" onClick={handleDateConfirm}>Confirm Dates</button>
+                </form>
             </div>
         </div>
     );
